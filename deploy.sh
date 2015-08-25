4b9f9147b6e67b37ecbd01ae34eb906f5b369b6d
jpena23 <airpena23@aol.com>
2015-08-25 14:27:29 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,6 @@
 # by John Pena | August 24, 2015
 #
 
-# list of dependencies / htdocs directory
-dep_list = 'git curl openssl php php-mysql mysql mysql-server'
-app_dir="/var/www/html"
-
 # this function will check for exit code and break out upon error
 check_exit() {
     exit_code = $?
@@ -23,28 +19,39 @@ check_exit() {
     fi
 }
 
-# check if mysql is installed, get password
-val = `rpm -qa |grep mysql`
+# list of dependencies / htdocs directory
+dep_list='git curl openssl php php-mysql mysql mysql-server'
+app_dir="/var/www/html/rest-api-sample-app-php"
+
+# remove directory if it exists to start clean and avoid errors
+if [ -d $app_dir ]; then
+    rm -rf $app_dir
+fi
+
+mkdir $app_dir
+
+# check if mysql is installed already, get password
+val=`rpm -qa |grep mysql-server`
 if [ -n $val ]; then
-    echo "please enter your mysql password: "
+    echo -n "please enter your mysql password: "
     read pw
 fi
 
 # flush firewall rules
 echo "flushing firewall rules..."
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -t nat -F
-iptables -t mangle -F
-iptables -F
-iptables -X 
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -F
+sudo iptables -X 
 
 # install dependencies, download paypal app setup via git
 echo "installing dependencies and downloading app..."
 sudo yum install $dep_list -y
-git clone https://github.com/paypal/rest-api-sample-app-php
-curl -sS https://getcomposer.org/installer | php -- --install-dir=$app_dir/rest-api-sample-app-php
+git clone https://github.com/paypal/rest-api-sample-app-php $app_dir
+curl -sS https://getcomposer.org/installer | php -- --install-dir=$app_dir
 
 # run update composer
 echo "updating composer..."
@@ -63,13 +70,13 @@ echo "create database paypal_pizza_app" | mysql -u $USER -p
 
 # if mysql is installed already, puts in user pw, otherwise puts in default blank pw
 if [ -n "$pw" ]; then
-    sed -i '20s/root/$pw/' bootstrap.php
+    sed -i '20s/root/$pw/' $app_dir/app/bootstrap.php
 else
-    sed -i '20s/root//' bootstrap.php
+    sed -i '20s/root//' $app_dir/app/bootstrap.php
 fi
 
 # creating neccesary tables in mysql
-cd $app_dir/rest-api-sample-app-php
+cd $app_dir/install
 php create_tables.php
 
 echo "Finished! Paypal Pizza App Installed and Ready To Use."
